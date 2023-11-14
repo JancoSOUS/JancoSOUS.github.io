5f18f2aff4a1fb24fea5e611b16b41c1120875ae
GitHub Action <action@github.com>
2023-11-14 22:24:04 +0000

    Auto-deploy to GitHub Pages

--- buurtwag/firebase-messaging-sw.js
+++ buurtwag/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.10.0/firebase-messaging-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
 
 firebase.initializeApp({
     apiKey: "AIzaSyC8FsxZw5yqH7sq0QjZHvRGs_HjP4nwi4U",
